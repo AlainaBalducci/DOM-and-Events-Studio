79f336dd14702b0b88080f069b6323bba4c6380b
Alaina Balducci <alainabalducci@gmail.com>
2023-02-16 19:00:01 -0600

    added more buttons and functionality to the rocket ship

--- scripts.js
+++ scripts.js
@@ -5,23 +5,74 @@ window.addEventListener('load', function () {
  const shuttleB = document.getElementById('shuttleBackground');
  const shuttleHeight = document.getElementById('spaceShuttleHeight');
  const takeOffButton = document.getElementById('takeoff');
+
+
  takeOffButton.addEventListener('click', function() {
  let response = window.confirm("Confirm that the shuttle is ready for takeoff");
  if (response) {
   paragraph.innerHTML = 'Shuttle in flight.';
   shuttleB.style.backgroundColor = 'blue';
-  shuttleHeight.innerHTML = 10000;
+  shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
  }
 })
 
 const landButton = document.getElementById('landing');
 landButton.addEventListener('click', function() {
- let newResponse = window.alert('The shuttle is landing. Landing gear engaged');
+ window.alert('The shuttle is landing. Landing gear engaged!');
   paragraph.innerHTML = 'The shuttle has landed.';
   shuttleB.style.backgroundColor = 'green';
-  shuttleHeight.innerHTML = 0; 
+  shuttleHeight.innerHTML = "0"; 
+})
+
+const abortMissButton = document.getElementById('missionAbort');
+abortMissButton.addEventListener('click', function () {
+ let response = window.confirm('Confirm that you want to abort the mission.');
+ if (response) {
+  paragraph.innerHTML = 'Mission aborted';
+  shuttleB.style.backgroundColor = 'green';
+  shuttleHeight.innerHTML = "0";
+  rocket.style.left = '300px';
+ rocket.style.right = '0px';
+ rocket.style.bottom = '0px';
+ rocket.style.top = '250px';
+ }
+})
+
+const upButton = document.getElementById('up');
+const downButton = document.getElementById('down');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+const rocket = document.getElementById('rocket');
+rocket.style.position = 'absolute';
+rocket.style.left = '300px';
+rocket.style.right = '0px';
+rocket.style.bottom = '0px';
+rocket.style.top = '250px';
+
+upButton.addEventListener('click', function () {
+ shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML)+10000;
+ let movement = parseInt(rocket.style.top) - 10 +'px';
+ rocket.style.top = movement;
+ console.log(movement)
+})
+
+downButton.addEventListener('click', function () {
+ shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML)-10000;
+ let movement = parseInt(rocket.style.top) + 10 +'px';
+ rocket.style.top = movement;
+ console.log(movement)
 })
 
 
+leftButton.addEventListener('click', function() {
+ let movement = parseInt(rocket.style.left) - 10 +'px';
+ rocket.style.left = movement;
+ console.log(movement)
+})
+
+rightButton.addEventListener('click', function() {
+ let movement = parseInt(rocket.style.left) + 10 +'px';
+ rocket.style.left = movement;
+})
 
 })
